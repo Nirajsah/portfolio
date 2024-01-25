569bcf4ec83ab1de8da17556bc8f82efcd98dc80
Nirajsah <nirajsahy4aj@gmail.com>
2024-01-25 17:58:27 +0530

    Add handleDownload function to AboutPage.js

--- src/components/Navbar/AboutPage.js
+++ src/components/Navbar/AboutPage.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Niraj from '../../assets/nirajsah.webp'
 import { TabContext } from '../TabProvider'
-
 const About = () => {
   const { handleTabChange } = React.useContext(TabContext) ?? {}
   const handleClick = (Tab) => {
     handleTabChange(Tab)
   }
+  const handleDownload = () => {
+    const resumeUrl =
+      'https://drive.google.com/file/d/1K4abaYrTWXlcfsdH4pQE8BIgFt_ESthZ/view?usp=sharing'
+    const link = document.createElement('a')
+    link.href = resumeUrl
+    link.download = 'Niraj_Sah_CV.pdf'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
   return (
     <div className="flex justify-center overflow-auto h-screen lg:mx-[6rem] rounded-[40px] w-auto lg:h-[650px]">
-      <div className="flex my-[1.5rem] justify-between mt-[50px] md:mt-0 px-[20px] lg:h-[400px] max-w-[840px] gap-4 items-center w-full flex-col md:flex-row lg:my-[3rem]">
+      <div className="flex my-[1.5rem] md:justify-between mt-[50px] md:mt-0 px-[20px] lg:h-[400px] max-w-[840px] gap-4 items-center w-full flex-col md:flex-row lg:my-[3rem]">
         <div className="border-[7px] flex flex-none object-cover md:border-[10px] lg:border-[18px] drop-shadow-2xl border-white rounded-full w-[280px] md:w-[300px] lg:w-[340px] lg:h-[340px]">
           <img
             className="w-full rounded-full h-full object-cover"
@@ -28,8 +37,11 @@ const About = () => {
             actively exploring the realms of Neural Networks and Machine
             Learning.
           </p>
-          <div className="flex mt-[1rem] gap-2">
-            <button className="duration-300 font-semibold hover:scale-105 ease-in-out px-3 md:px-7 md:py-2 py-2 hover:bg-[#007ced] hover:text-cyan-50 drop-shodow-lg rounded-full text-sm border-2 border-[#007ced]">
+          <div className="flex flex-col md:flex-row mt-[1rem] gap-2">
+            <button
+              onClick={handleDownload}
+              className="duration-300 font-semibold hover:scale-105 ease-in-out px-3 md:px-7 md:py-2 py-2 hover:bg-[#007ced] hover:text-cyan-50 drop-shodow-lg rounded-full text-sm border-2 border-[#007ced]"
+            >
               Download CV
             </button>
             <button
